59f9e60258f0e458fac76d7fd519939a8b6f36a8
Edwin Guerrero <edwincas1012@gmail.com>
2025-12-11 16:26:06 -0300

    Feat(routes): implementando rotas

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,15 @@
 
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Models\User;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\NoteController;
+
+
+// Route::get('/users', [UserController::class, 'index']);
 
 Route::apiResource('users', UserController::class);
+
+Route::apiResource('notes', NoteController::class);
